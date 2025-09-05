6d2e2ec56d1630e3c9e550ad996d29b7bc191788
Hema <157871013+kusu-hema@users.noreply.github.com>
2025-09-05 18:35:35 +0530

    Changes updated

---
+++ config/DatabaseQuerys.js
@@ -0,0 +1,37 @@
+
+
+
+
+// category Table --------------
+// CREATE TABLE category (
+//   sno SERIAL PRIMARY KEY,
+//   category_name VARCHAR(100) NOT NULL,
+//   image TEXT
+// );
+
+
+
+
+// Customers Table -------------
+// CREATE TABLE customers (
+//   id SERIAL PRIMARY KEY,
+//   name VARCHAR(100) NOT NULL,
+//   phone VARCHAR(20),
+//   start TIMESTAMP,
+//   end_date TIMESTAMP,
+//   type VARCHAR(50),
+//   location VARCHAR(100),
+//   status VARCHAR(20)
+// );
+
+
+
+
+// products Table --------------
+// CREATE TABLE products (
+//   sno SERIAL PRIMARY KEY,
+//   image TEXT,
+//   product VARCHAR(100) NOT NULL,
+//   category VARCHAR(100),
+//   action VARCHAR(100)
+// );
