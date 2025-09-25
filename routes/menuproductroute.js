a296b709d8296871200daa1028d0eb80d21c910f
Hema <157871013+kusu-hema@users.noreply.github.com>
2025-09-25 19:11:16 +0530

    changes updated

--- routes/menuproductroute.js
+++ routes/menuproductroute.js
@@ -5,16 +5,16 @@ const controller = require('../controller/menuproductcontroller');
 // CRUD APIs
 router.get('/', controller.getProducts);
 
-
+// get product by id 
 router.get('/:id', controller.getProductById);
 
-
+// Post api 
 router.post('/', controller.createProduct);
 
-
+// update api
 router.put('/:id', controller.updateProduct);
 
-
+// delete api
 router.delete('/:id', controller.deleteProduct);
 
 
